e1c7ddbf194359ee43b8407be8ed20c582d6f85e
mzj <825241378@qq.com>
2020-05-02 00:23:38 +0800

    逆向生成工具

---
+++ src/main/java/com/dengfeng/mapper/RoleMapper.java
@@ -0,0 +1,7 @@
+package com.dengfeng.mapper;
+
+import com.dengfeng.my.mapper.BaseMapper;
+import com.dengfeng.pojo.Role;
+
+public interface RoleMapper extends BaseMapper<Role> {
+}
---
+++ src/main/java/com/dengfeng/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.dengfeng.mapper;
+
+import com.dengfeng.my.mapper.BaseMapper;
+import com.dengfeng.pojo.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
